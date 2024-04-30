2b08866ecbde9d25a02c72039c7149b44626c849
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-04-30 17:31:16 +0900

    Fix: rubocop, erblint

--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -1,13 +1,17 @@
 class Posts::CommentsController < ApplicationController
   before_action :require_login
 
-  def create
-    @comment = current_user.comments.build(comment_params)
-    @comment.save
+  def show
+    @comment = current_user.comments.find(params[:id])
   end
 
   def edit
     @comment = current_user.comments.find(params[:id])
+  end
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
   end
 
   def update
@@ -23,16 +27,13 @@ class Posts::CommentsController < ApplicationController
     # end
   end
 
-  def show
-    @comment = current_user.comments.find(params[:id])
-  end
-
   def destroy
     @comment = current_user.comments.find(params[:id])
     @comment.destroy!
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:body).merge(post_id: params[:post_id])
   end
